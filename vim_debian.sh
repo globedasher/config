be71142acffc806df4b85c6b61094afef11a7bf4
globedasher <globedasher@loadbearer.homebase>
2017-02-22 20:52:11 -0800

    more updates to vim_debian.sh

--- vim_debian.sh
+++ vim_debian.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#TODO: Question?! Can/should I run this as root and avoid the sudo calls?
+# TODO: Can/should I run this as root and avoid the sudo calls?
 
-# Update! Maybe I should add a section to ensure the non-free repos for debian
+# TODO: Maybe I should add a section to ensure the non-free repos for debian
 # are added.
+
+# Update! 
+echo "Update"
 sudo apt-get update
 
 # Remove all unneeded versions of vim...
@@ -15,15 +18,25 @@ sudo apt-get remove 	vim \
 			vim-gui-common \
 			vim-nox\
 
+# For the broadcom BCM43142  wifi on my laptop. The non-free debian repos must
+# be enabled.
+sudo apt-get install broadcom-sta-dkms
+
 # Install my applications and dependencies for Vim.
-sudo apt-get install -y	tmux \
-			stterm \
+sudo apt-get install -y	firefox-esr\
+			tmux \
+			xorg \
 			xmonad \
+			stterm \
+			suckless-tools \
 			npm \
 			zsh \
 			htop \
 			tree \
+			connman \
+			rfkill \
 			git \
+			gimp \
 			python-dev \
     			python3-dev \
 			ruby-dev \
@@ -41,16 +54,17 @@ sudo apt-get install -y	tmux \
 			libxpm-dev \
 			libxt-dev \
 
-# TODO: create a shell change here to use zsh, but make the call declaritive...
-sudo chsh -s `which zsh`
+# TODO: Install MongoDB...
+#sudo apt-get mongodb-org
+
+# Change shell to use zsh
+chsh -s `which zsh`
 
 # Install nodejs, bower and nodemon.
 sudo npm install -g nodejs bower nodemon
 
-# TODO: Install MongoDB...
-
 # Create the missing Debian symlink.
-sudo ln -s /usr/bin/node /usr/bin/nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 # Change to home directory and clone the vim repository
 cd ~
@@ -71,10 +85,10 @@ cd ~/.vim
 	    --prefix=/usr \
 
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
-
-cd ~/.vim
 sudo make install
 
+
+# Post install config for Vim
 # Create the file type plugin folder.
 mkdir ~/.vim/ftplugin/
 cp .myconfig/ftplugin/* ~/.vim/ftplugin/

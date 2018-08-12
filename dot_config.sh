c1efee6a2fce672c3fa8e9730b339e075f5cd809
globedasher <globe.dasher@gmail.com>
2018-08-12 12:23:21 -0700

    Added packages and update config transfer.

--- dot_config.sh
+++ dot_config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Copy all files from config to the home folder
-cp config/* ~
+cp -r config/.* ~
--- packages.sh
+++ packages.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pacman -S git tmux vim ranger chromium zsh zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions
+pacman -S git tmux \
+    vim \
+    ranger \
+    chromium \
+    zsh \
+    zsh-syntax-highlighting \
+    zsh-history-substring-search \
+    zsh-autosuggestions \
+    nmap \
